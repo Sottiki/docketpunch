63b61a81636e75fb3cd3566ddde6d3a02a72fbfc
Sottiki <81673075+Sottiki@users.noreply.github.com>
2025-10-13 01:04:24 +0900

    タイポ修正

--- cmd/clear.go
+++ cmd/clear.go
@@ -10,31 +10,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// clrearCmd represents the clrear command
-var clrearCmd = &cobra.Command{
-	Use:   "clrear",
+// clearCmd represents the clear command
+var clearCmd = &cobra.Command{
+	Use:   "clear",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+a`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("clrear called")
+		fmt.Println("clear called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(clrearCmd)
+	rootCmd.AddCommand(clearCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// clrearCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// clearCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// clrearCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// clearCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
